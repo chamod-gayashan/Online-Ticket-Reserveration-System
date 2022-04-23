4010059123e1fbe27c8ea55586de12e3d129609b
Chamod Gayashan <gayashan113@gmail.com>
2022-04-24 00:52:17 +0530

    first commit

---
+++ medallion.sql
@@ -0,0 +1,304 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 14, 2022 at 07:32 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.0.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `medallion`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accomodation`
+--
+
+CREATE TABLE `accomodation` (
+  `acc_id` int(11) NOT NULL,
+  `acc_type` varchar(35) NOT NULL,
+  `acc_price` double NOT NULL,
+  `acc_slot` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `accomodation`
+--
+
+INSERT INTO `accomodation` (`acc_id`, `acc_type`, `acc_price`, `acc_slot`) VALUES
+(1, 'Sitting', 350, 30),
+(2, 'Economy A', 390, 30),
+(3, 'Economy B', 390, 30),
+(4, 'Tourist', 490, 30),
+(5, 'Cabin', 600, 30),
+(6, 'Deluxe', 700, 30);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booked`
+--
+
+CREATE TABLE `booked` (
+  `book_id` int(11) NOT NULL,
+  `book_by` varchar(50) NOT NULL,
+  `book_contact` varchar(15) NOT NULL,
+  `book_address` varchar(100) NOT NULL,
+  `book_name` varchar(100) NOT NULL,
+  `book_age` int(11) NOT NULL,
+  `book_gender` varchar(15) NOT NULL,
+  `book_departure` date NOT NULL,
+  `dest_id` int(11) NOT NULL,
+  `acc_id` int(11) NOT NULL,
+  `origin_id` int(11) NOT NULL,
+  `book_tracker` varchar(35) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `booked`
+--
+
+INSERT INTO `booked` (`book_id`, `book_by`, `book_contact`, `book_address`, `book_name`, `book_age`, `book_gender`, `book_departure`, `dest_id`, `acc_id`, `origin_id`, `book_tracker`) VALUES
+(2, 'Pethum Rasanjana', '0332274737', 'Rathnapura', 'M.K. Chanuka Nayanajith Mandawela', 23, 'Male', '2022-03-24', 2, 6, 4, '622f84e6a4ac2'),
+(3, 'Chanuka Nayanajith', '0715226008', 'Nalla road,Hakurukumbura, Mirigama', 'Isuri Mapa', 23, 'Female', '2022-03-16', 4, 4, 6, '622f8592c2328'),
+(4, 'Chanuka Nayanajith', '0715226008', 'Nalla road,Hakurukumbura, Mirigama', 'A.H.P. Rasanjana', 22, 'Male', '2022-03-16', 4, 4, 6, '622f8592c2328');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `destination`
+--
+
+CREATE TABLE `destination` (
+  `dest_id` int(11) NOT NULL,
+  `dest_destination` varchar(35) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `destination`
+--
+
+INSERT INTO `destination` (`dest_id`, `dest_destination`) VALUES
+(1, 'Nittabuwa'),
+(2, 'Gampaha'),
+(3, 'Kaduwela'),
+(4, 'Malabe');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `origin`
+--
+
+CREATE TABLE `origin` (
+  `origin_id` int(11) NOT NULL,
+  `origin_desc` varchar(35) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `origin`
+--
+
+INSERT INTO `origin` (`origin_id`, `origin_desc`) VALUES
+(1, 'Baby'),
+(2, 'Child'),
+(3, 'Twen'),
+(4, 'Teenager'),
+(5, 'Young Adult'),
+(6, 'Elder');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status`
+--
+
+CREATE TABLE `status` (
+  `stat_id` int(11) NOT NULL,
+  `stat_desc` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `status`
+--
+
+INSERT INTO `status` (`stat_id`, `stat_desc`) VALUES
+(1, 'Paid'),
+(2, 'Refunded');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `trans_id` int(11) NOT NULL,
+  `trans_time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `trans_payment` double NOT NULL,
+  `trans_passenger` varchar(100) NOT NULL,
+  `trans_age` int(11) NOT NULL,
+  `trans_gender` varchar(15) NOT NULL,
+  `acc_id` int(11) NOT NULL,
+  `origin_id` int(11) NOT NULL,
+  `dest_id` int(11) NOT NULL,
+  `stat_id` int(11) DEFAULT 1,
+  `trans_refunded` tinyint(4) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `user_account` varchar(50) NOT NULL,
+  `user_password` varchar(35) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `user_account`, `user_password`) VALUES
+(1, 'Chanuka0321', '20@TC*sa');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `accomodation`
+--
+ALTER TABLE `accomodation`
+  ADD PRIMARY KEY (`acc_id`);
+
+--
+-- Indexes for table `booked`
+--
+ALTER TABLE `booked`
+  ADD PRIMARY KEY (`book_id`),
+  ADD KEY `dest_id` (`dest_id`,`acc_id`),
+  ADD KEY `acc_id` (`acc_id`),
+  ADD KEY `origin_id` (`origin_id`);
+
+--
+-- Indexes for table `destination`
+--
+ALTER TABLE `destination`
+  ADD PRIMARY KEY (`dest_id`);
+
+--
+-- Indexes for table `origin`
+--
+ALTER TABLE `origin`
+  ADD PRIMARY KEY (`origin_id`);
+
+--
+-- Indexes for table `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`stat_id`);
+
+--
+-- Indexes for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`trans_id`),
+  ADD KEY `acc_id` (`acc_id`,`origin_id`,`dest_id`,`stat_id`),
+  ADD KEY `origin_id` (`origin_id`),
+  ADD KEY `dest_id` (`dest_id`),
+  ADD KEY `stat_id` (`stat_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `accomodation`
+--
+ALTER TABLE `accomodation`
+  MODIFY `acc_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `booked`
+--
+ALTER TABLE `booked`
+  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `destination`
+--
+ALTER TABLE `destination`
+  MODIFY `dest_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `origin`
+--
+ALTER TABLE `origin`
+  MODIFY `origin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `status`
+--
+ALTER TABLE `status`
+  MODIFY `stat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `trans_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `booked`
+--
+ALTER TABLE `booked`
+  ADD CONSTRAINT `booked_ibfk_1` FOREIGN KEY (`dest_id`) REFERENCES `destination` (`dest_id`),
+  ADD CONSTRAINT `booked_ibfk_2` FOREIGN KEY (`acc_id`) REFERENCES `accomodation` (`acc_id`),
+  ADD CONSTRAINT `booked_ibfk_3` FOREIGN KEY (`origin_id`) REFERENCES `origin` (`origin_id`);
+
+--
+-- Constraints for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`acc_id`) REFERENCES `accomodation` (`acc_id`),
+  ADD CONSTRAINT `transaction_ibfk_2` FOREIGN KEY (`origin_id`) REFERENCES `origin` (`origin_id`),
+  ADD CONSTRAINT `transaction_ibfk_3` FOREIGN KEY (`dest_id`) REFERENCES `destination` (`dest_id`),
+  ADD CONSTRAINT `transaction_ibfk_4` FOREIGN KEY (`stat_id`) REFERENCES `status` (`stat_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
